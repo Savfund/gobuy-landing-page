97e665a3aa6445008af11b2ae1f0f489d0672c26
codinkage <husseinmubarak500@gmail.com>
2025-05-12 07:02:06 +0100

    feat: mapping main-service url to main-service page

--- src/constants.ts
+++ src/constants.ts
@@ -1,18 +1,15 @@
-// Map main service routes to main service pages
-
 export const RoutePaths = {
   DEFAULT: "/",
-  LOGIN: "/login",
-  REGISTER: "/register",
-  VERIFY_ACCOUNT: "/verify-account",
-  FORGOT_PASSWORD: "/forgot-password",
-  CHANGE_PASSWORD: "/change-password",
+  APP_URI: "http://localhost:3001/",
   PRIVACY_POLICY: "/privacy-policy",
   TERMS_OF_SERVICE: "/terms-of-service",
   SETTINGS: "/settings",
   SERVICES: "/services",
   NOTIFICATIONS: "/notifications",
-  get PROFILE() {
-    return `${this.SETTINGS}/profile`;
+  get LOGIN() {
+    return `${this.APP_URI}/login`;
+  },
+  get REGISTER() {
+    return `${this.APP_URI}/register`;
   },
 };
