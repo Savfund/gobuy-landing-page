220b597d0fbc8715b8f2eb1e75e69191a700f5d1
codinkage <husseinmubarak500@gmail.com>
2025-05-15 17:09:04 +0100

    deps: vercel speed insight code added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,6 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "Gobuy platform",
@@ -23,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
