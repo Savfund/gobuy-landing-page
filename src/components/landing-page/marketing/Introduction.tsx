6a4de755396fde61e86bd1a62cbb897ecca104b5
codinkage <husseinmubarak500@gmail.com>
2025-05-15 18:15:35 +0100

    chore(ui): Introduction page ui styling modifications

--- src/components/landing-page/marketing/Introduction.tsx
+++ src/components/landing-page/marketing/Introduction.tsx
@@ -6,18 +6,22 @@ import Image from "next/image";
 export default function Introduction() {
   return (
     <Container padding="px-10" margin="mt-14">
-      <Flex alignItems="start" justifyContent="between">
-        <Container size="w-2/5">
+      <Flex
+        alignItems="start"
+        justifyContent="between"
+        classname="flex-col-reverse md:flex-row"
+      >
+        <Container size="md:w-2/5">
           <div>
-            <h1 className="text-[45px] text-eminence">
+            <h1 className="text-4xl md:text-[45px] text-eminence">
               The Easy Way To Use Your PI.
             </h1>
-            <p className="text-xl mt-4">
+            <p className="text-lg md:text-xl mt-2 md:mt-4 text-justify text-grey-scale">
               Don&apos;t let your crypto sit idle. Gobuy transforms it into the
               services you need.
             </p>
             <Button
-              className="w-2/4 text-center mt-4 py-3 rounded-lg"
+              className="w-full md:w-2/4 text-center mt-8 md:mt-4 py-3 rounded-lg"
               url={RoutePaths.REGISTER}
               asLink
             >
