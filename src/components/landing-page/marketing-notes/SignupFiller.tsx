847b9ee8332b7eb85549dcdadb02bd58272af575
codinkage <husseinmubarak500@gmail.com>
2025-05-15 17:36:46 +0100

    chore(ui): styling SignupFiller component

--- src/components/landing-page/marketing-notes/SignupFiller.tsx
+++ src/components/landing-page/marketing-notes/SignupFiller.tsx
@@ -3,24 +3,19 @@ import { RoutePaths } from "@/constants";
 
 export default function SignUpFiller() {
   return (
-    <Container
-      background="primary"
-      padding="p-20"
-      margin="mx-10"
-      outline="rounded-xl"
-    >
-      <Container size="w-1/2" margin="mx-auto">
+    <Container padding="md:p-20" margin="mx-5 md:mx-10" outline="rounded-xl">
+      <Container size="md:w-1/2" margin="mx-auto">
         <Flex alignItems="center" classname="flex-col">
-          <h1 className="text-4xl text-center mb-3 font-bold">
+          <h1 className="text-2xl md:text-4xl text-center mb-3 font-bold">
             Maximize Your PI: Get Data & Airtime, Instantly.
           </h1>
-          <p className="text-lg w-[90%] text-center mt-3 mb-5 font-light">
+          <p className="text-[14px] md:text-lg md:w-[90%] text-center md:mt-3 mb-5 font-light text-grey-scale">
             Join gobuy and experience the convenience of converting your
             cryptocurrency into valuable mobile subscriptions.
           </p>
           <Button
             url={RoutePaths.REGISTER}
-            className="w-2/4 text-center py-3 rounded-lg"
+            className="w-full md:w-2/4 text-center py-1 md:py-3 rounded-lg"
             asLink
           >
             Get Started Now
