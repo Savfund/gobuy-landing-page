00bd81e13ab12732e45ac50d5cc4b24d1436c658
codinkage <husseinmubarak500@gmail.com>
2025-05-21 07:37:10 +0100

    fix(ui/mobile): enabled removal of scrolling behaviour if menu toggle is opened

--- src/components/navigation/mobile/index.tsx
+++ src/components/navigation/mobile/index.tsx
@@ -3,7 +3,7 @@
 import { Container, Logo } from "@/common";
 import { useDimensions } from "@/hooks/useDimensions";
 import { motion } from "framer-motion";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { background, nav, sidebarVariants } from "../styles/mobile";
 import MenuToggle from "./MenuToggle";
 import Navigation from "./Navigation";
@@ -16,6 +16,17 @@ function MobileNavigation() {
   const handleMenuToggle = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
 
   return (
     <>
