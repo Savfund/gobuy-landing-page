d49c3d8d9fba7a6a8ff915abdc896e3d9c6ef5ee
codinkage <husseinmubarak500@gmail.com>
2025-05-21 07:45:20 +0100

    ...

--- src/components/navigation/mobile/index.tsx
+++ src/components/navigation/mobile/index.tsx
@@ -3,7 +3,7 @@
 import { Container, Logo } from "@/common";
 import { useDimensions } from "@/hooks/useDimensions";
 import { motion } from "framer-motion";
-import { useRef, useState, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { background, nav, sidebarVariants } from "../styles/mobile";
 import MenuToggle from "./MenuToggle";
 import Navigation from "./Navigation";
