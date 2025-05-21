d65b1e78b02edcb0d0e90509eb2acc76f632e76c
codinkage <husseinmubarak500@gmail.com>
2025-05-21 07:11:04 +0100

    fix: Bug fixes

--- src/components/navigation/mobile/MenuItem.tsx
+++ src/components/navigation/mobile/MenuItem.tsx
@@ -1,6 +1,6 @@
 import { Container, Flex } from "@/common";
 import { motion } from "framer-motion";
-import { Link } from "react-scroll";
+import Link from "next/link";
 import SocialButtons from "../SocialButtons";
 import { itemVariants, listItem } from "../styles/mobile";
 
@@ -9,10 +9,10 @@ const MenuItem = () => {
     <motion.li style={listItem} variants={itemVariants}>
       <Flex direction="col" gap="space-y-3">
         <Container classname="mb-5 text-lg">
-          <Link to="about-us" duration={500} smooth>
+          <Link href="#about">
             <h1 className="mb-2 text-xl">About us</h1>
           </Link>
-          <Link to="features" duration={500} smooth>
+          <Link href="#features">
             <h1 className="text-xl">Features</h1>
           </Link>
         </Container>
