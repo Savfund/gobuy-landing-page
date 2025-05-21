d6943921af8621ec579a28673d50ed6ddd1d0559
codinkage <husseinmubarak500@gmail.com>
2025-05-21 15:05:58 +0100

    chore: ...

--- src/data/marketing.tsx
+++ src/data/marketing.tsx
@@ -1,9 +1,9 @@
 export const marketing = {
   features: [
-    // {
-    //   heading: "Connectivity, Instantly Powered by PI",
-    //   note: "CoinBuy eliminates the wait. Convert your PI to data and airtime subscriptions in seconds, ensuring you stay connected whenever you need it. No more complicated exchanges or delays – just seamless access to essential mobile services.",
-    // },
+    {
+      heading: "Connectivity, Instantly Powered by PI",
+      note: "CoinBuy eliminates the wait. Convert your PI to data and airtime subscriptions in seconds, ensuring you stay connected whenever you need it. No more complicated exchanges or delays – just seamless access to essential mobile services.",
+    },
     {
       heading: "Unlock Real-World Value from Your PI",
       note: "Your PI has real-world utility with CoinBuy. We bridge the gap between cryptocurrency and everyday needs, allowing you to easily convert your digital assets into tangible benefits. Get the most out of your PI by using it for practical, essential services.",
