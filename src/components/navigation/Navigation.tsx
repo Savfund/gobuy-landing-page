d431766dab92a56f9e67f91b132ae7d937f374e7
codinkage <husseinmubarak500@gmail.com>
2025-07-11 13:16:51 +0100

    ...

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -2,16 +2,13 @@ import { Container } from "@/common";
 import Footer from "../landing-page/Footer";
 import DesktopNavigation from "./desktop";
 import MobileNavigation from "./mobile";
+import { PropsWithChildren } from "react";
 
 export type ToggleInterface = {
   toggle: () => void;
 };
 
-export default function Navigation({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Navigation({ children }: PropsWithChildren) {
   return (
     <>
       <Container>
