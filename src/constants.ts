08631d29bf595052f9d93bd793f43fafd52b9d85
codinkage <husseinmubarak500@gmail.com>
2025-07-23 07:57:17 +0100

    ...

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,16 @@
 export const RoutePaths = {
   DEFAULT: "/",
-  APP_URI: "http://localhost:3001",
+  LOCAL_APP_URI: "http://localhost:3000",
   PRIVACY_POLICY: "/privacy-policy",
   TERMS_OF_SERVICE: "/terms-of-service",
   SETTINGS: "/settings",
   SERVICES: "/services",
   NOTIFICATIONS: "/notifications",
+  PROD_APP_URI: `https://app.subnownow.com`,
   get LOGIN() {
-    return `${this.APP_URI}/login`;
+    return `${this.PROD_APP_URI}/login`;
   },
   get REGISTER() {
-    return `${this.APP_URI}/register`;
+    return `${this.PROD_APP_URI}/register`;
   },
 };
-
-
